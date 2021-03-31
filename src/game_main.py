d72276ffaef957b8c94146092741aab389277d05
Sturmwolke <frantalmt@gmail.com>
2021-04-01 01:14:08 +0200

    added test map

--- src/game_main.py
+++ src/game_main.py
@@ -22,7 +22,7 @@ def launch_game():
     pygame.mixer.pre_init(frequency=44100,size=-16,channels=2, buffer=2048)
     pygame.init()
     screen = pygame.display.set_mode(config.WINDOW_DIMENSIONS)
-    gameMap = pytmx.load_pygame("data/maps/test-map-csv.tmx")
+    gameMap = pytmx.load_pygame("data/maps/test-map.tmx")
     running = True
     button = pygame_additions.button(40,40,100,100)
     button.set_action(hallo_welt)
