cc347368db200b6b92566cc98530bd8ceb66bc84
Häußermann Ben <bh070@hdm-stuttgart.de>
2021-03-31 21:50:16 +0200

    fixed issues

--- src/game_objects/Guard.py
+++ src/game_objects/Guard.py
@@ -1,11 +1,11 @@
 from ..superclasses import Actor
-from .helper\Section import Section
-from .helper\Point import Point
+from .helper.Section import *
+from .helper.Point import *
 import math
 
 class Guard(Actor.Actor):
 
-    def __init__(self):
+    def __init__(self,pos):
         super().__init__()
 
         self.pos = pos
@@ -19,17 +19,17 @@ class Guard(Actor.Actor):
 
     def raycast(self, degree, length, walls):
 
-
+        end = Point(200,200)
         ray = Section((self.pos.x,self.pos.y),(end.x,end.y))
         m = (ray.endPoint[0]-ray.startPoint[0])/(ray.endPoint[1]-ray.startPoint[1])
         intersections = []
         for wall in walls:
             m_wall = (wall.endPoint[0]-wall.startPoint[0])/(wall.endPoint[1]-wall.startPoint[1])
-            intersection = Point((-ray.startPoint[1]+wall.startPoint[1])/(m-m_wall),(ray.startPoint[1]+m*pointX))
+            intersection = Point((-ray.startPoint[1]+wall.startPoint[1])/(m-m_wall),(ray.startPoint[1]+m*(-ray.startPoint[1]+wall.startPoint[1])/(m-m_wall)))
             distance = distance(ray.startPoint,ray.endPoint)
             if distance > length:
                 intersection = Point(999,999)
-            if !(wall.startPoint.x < intersection.x) and (wall.endPoint > intersection.x):
+            if not (wall.startPoint.x < intersection.x) and (wall.endPoint > intersection.x):
                 intersection = Point(999,999)
             tup = (distance,intersection)
             intersections.append(tup)
