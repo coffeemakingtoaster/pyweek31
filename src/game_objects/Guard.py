8b9b939abd60e27bca17dffbb70cdf5d900e1910
Häußermann Ben <bh070@hdm-stuttgart.de>
2021-03-31 21:10:29 +0200

    Added Raycast Logic

--- src/game_objects/Guard.py
+++ src/game_objects/Guard.py
@@ -1,11 +1,48 @@
 from ..superclasses import Actor
+from .helper\Section import Section
+from .helper\Point import Point
+import math
 
 class Guard(Actor.Actor):
 
     def __init__(self):
         super().__init__()
-        self.x = 200
-        self.y = 200
+        self.pos = Point()
+        self.goalPos = Point()
 
     def update(self):
         pass
+
+    def raycast(self, degree, length, walls):
+
+
+        ray = Section((self.pos.x,self.pos.y),(end.x,end.y))
+        m = (ray.endPoint[0]-ray.startPoint[0])/(ray.endPoint[1]-ray.startPoint[1])
+        intersections = []
+        for wall in walls:
+            m_wall = (wall.endPoint[0]-wall.startPoint[0])/(wall.endPoint[1]-wall.startPoint[1])
+            intersection = Point((-ray.startPoint[1]+wall.startPoint[1])/(m-m_wall),(ray.startPoint[1]+m*pointX))
+            distance = distance(ray.startPoint,ray.endPoint)
+            if distance > length:
+                intersection = Point(999,999)
+            if !(wall.startPoint.x < intersection.x) and (wall.endPoint > intersection.x):
+                intersection = Point(999,999)
+            tup = (distance,intersection)
+            intersections.append(tup)
+        intersections.sort(key=lambda tup: tup[1])
+        
+
+
+
+
+
+
+
+
+
+    def move(self):
+        #movement
+        return goalPos
+
+    def distance(a,b):
+        return sqrt((a.x - b.x)**2 + (a.y - b.y)**2)
