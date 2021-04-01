a051f25b8e178a1d443f5547434db5e47d1b1b1b
Sarah Mauff <sm236@hdm-stuttgart.de>
2021-04-01 17:55:47 +0200

    Winning sequence added

--- src/Keycards.py
+++ src/Keycards.py
@@ -4,13 +4,15 @@ from . import Render
 
 class Keycards():
 
+    keycard_spawns = [{"x_spawn":1568, "y_spawn":2275}, {"x_spawn":949, "y_spawn":1194}, {"x_spawn":1881, "y_spawn":652}, {"x_spawn":1880, "y_spawn":1530}, {"x_spawn":573, "y_spawn":630}]
+    collect_counter = 0
 
     def __init__(self, assets):
         self.x = 40
         self.y = 40
         self.is_picked_up = False
         self.container = []
-        self.assets = assets
+        self.assets = assets 
     
     #container = []
 
@@ -19,16 +21,31 @@ class Keycards():
     
     def create_keycards(self):
         assets = self.assets
-        for i in range(10):
-            keycard_posx = random.randrange(1000)
-            keycard_posy = random.randrange(1000)
+        for i in range(3):
+            #keycard_posx = random.randrange(1000)
+            #keycard_posy = random.randrange(1000)
+            keycard_pos = random.choice(self.keycard_spawns)
+            keycard_posx = keycard_pos["x_spawn"]
+            keycard_posy = keycard_pos["y_spawn"]
             keycard_rect = assets['textures']['keycard'].get_rect()
             keycard_rect.center = (keycard_posx,keycard_posy)
             self.container.append({"x_cord":keycard_posx, "y_cord":keycard_posy, "collectable":True, "rect":keycard_rect})
         return self.container
     
     def keycard_player_collision(self, keycard_rect, player_rect):
-        for keycard in self.container:
-            if keycard["rect"].colliderect(player_rect):
-                print("collided")
-                keycard["collectable"] = False
+        if self.collect_counter <= 2:
+            print(self.collect_counter)
+            for keycard in self.container:
+                print("schleife")
+                if keycard["rect"].colliderect(player_rect):
+                    if keycard["collectable"] == True:
+                        print("collided")
+                        keycard["collectable"] = False
+                        self.collect_counter += 1
+                        print(self.collect_counter)
+        elif self.collect_counter >= 3:
+                print("to winning")
+                self.winning()
+
+    def winning(self):
+        print("You won")
