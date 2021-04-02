1d602e4769265b9b0c25f3002d283fc44a5346c5
Lars Flieger <a.purenetwork@gmail.com>
2021-04-03 00:22:44 +0200

    Added gras textures

---
+++ data/maps/gras.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="gras" tilewidth="16" tileheight="16" tilecount="11" columns="11">
+ <image source="../assets/mapsprite/gras.png" width="176" height="16"/>
+</tileset>
