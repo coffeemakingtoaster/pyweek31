d3e5b13cfeb0bb555e3303c79c27586b985308d7
Lars Flieger <a.purenetwork@gmail.com>
2021-04-02 23:16:30 +0200

    Fixed issue with relative path of maps

---
+++ data/maps/prison_inside.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="prison_inside" tilewidth="16" tileheight="16" tilecount="5" columns="5">
+ <image source="../assets/mapsprite/prison_inside.png" width="80" height="16"/>
+</tileset>
