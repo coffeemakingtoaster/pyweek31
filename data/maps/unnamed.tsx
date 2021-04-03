ae41f9e7e09add1deef1bbb62085d9456e18a311
Häußermann Ben <bh070@hdm-stuttgart.de>
2021-04-03 16:15:24 +0200

    player hiding and gameover

---
+++ data/maps/unnamed.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="unnamed" tilewidth="16" tileheight="16" tilecount="128" columns="32">
+ <image source="../assets/testing/unnamed.png" width="512" height="64"/>
+</tileset>
