c1a1e7b76dfed72f3303ae6b6ef9d337f88a080e
Lars Flieger <a.purenetwork@gmail.com>
2021-04-03 21:57:41 +0200

    Added spawnpoints to map

--- src/Logic.py
+++ src/Logic.py
@@ -39,6 +39,11 @@ class Logic():
         self.map = game_map
         self.collision_objects = []
         self.hiding_spots = []
+        self.mice_areas = []
+        self.keycards_spawnpoints = []
+        # MAX:
+        # self.keycards_spawnpoints[0].x and self.keycards_spawnpoints[0].y
+        # 
 
         # get all map trigger points
         self.get_map_trigger()
@@ -147,6 +152,14 @@ class Logic():
                         height = properties['height'] * (config.TILE_SIZE/16)
                         spot = pygame.Rect(x, y, width, height)
                         self.chests.append(Chest.Chest(spot))
+                    elif properties["name"] == "chest":
+                        x = properties['x'] * (config.TILE_SIZE/16)
+                        y = properties['y'] * (config.TILE_SIZE/16)
+                        width = properties['width'] * (config.TILE_SIZE/16)
+                        height = properties['height'] * (config.TILE_SIZE/16)
+                        spot = pygame.Rect(x, y, width, height)
+                    elif properties["name"] == "keycard_spawnpoint":
+                        self.keycards_spawnpoints.append(Point(properties['x'] * (config.TILE_SIZE/16), properties['y'] * (config.TILE_SIZE/16)))
 
 
     def refresh_walls(self):
--- src/game_objects/helper/Point.py
+++ src/game_objects/helper/Point.py
@@ -6,6 +6,8 @@ class Point():
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return "Point(" + str(self.x) + ", " + str(self.y) + ")"
 
     def to_our_point(point):
         return Point(point.x * (TILE_SIZE/16), point.y * (TILE_SIZE/16))
